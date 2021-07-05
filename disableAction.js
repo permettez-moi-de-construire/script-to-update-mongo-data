11ed77f38bedd44ae50c1c97c64f42e8132be1a5
rojo <rojorandriamanantsoa@gmail.com>
2021-07-05 15:50:01 +0300

    Fix query action mongodb

--- disableAction.js
+++ disableAction.js
@@ -1,91 +1,102 @@
-const { 
-    mongoConnect,
-    postgresPool
- } = require('./dbConnect')
+const { mongoConnect, postgresPool } = require("./dbConnect");
 
- const { 
-    getDeals
- } = require('./hubspot')
+const { getDeals } = require("./hubspot");
 
-const metaIds = [ 8, 9 ]
+const metaIds = [8, 9];
 
+const ONBOARDING_PIPELINE = process.env.ONBOARDING_PIPELINE;
+const FACILITATION_PIPELINE = process.env.FACILITATION_PIPELINE;
 
-const ONBOARDING_PIPELINE = process.env.ONBOARDING_PIPELINE
-const FACILITATION_PIPELINE = process.env.FACILITATION_PIPELINE
-
-mongoConnect().then(async collection => {    
-    /* OnBoarding { pipelineId: 1489583 } */
-    await disableActions(collection, +ONBOARDING_PIPELINE)
-    /* Facilitation { pipelineId: 1489647 } */
-    await disableActions(collection, +FACILITATION_PIPELINE)
-})
+mongoConnect().then(async (collection) => {
+  /* OnBoarding { pipelineId: 1489583 } */
+  await disableActions(collection, +ONBOARDING_PIPELINE);
+  /* Facilitation { pipelineId: 1489647 } */
+  await disableActions(collection, +FACILITATION_PIPELINE);
+});
 
 const disableActions = async (collection, pipelineId) => {
-    console.log('==================[DEBUT]==================');
-    const deals = await getDeals(pipelineId, false, 0);
-    console.log('[[ DEAL SIZE ]]', deals.length)
+  console.log("==================[DEBUT]==================");
+  const deals = await getDeals(pipelineId, false, 0);
+  console.log("[[ DEAL SIZE ]]", deals.length);
 
-    if(deals.length) {
-        // get association project deal
-        const dealIds = deals.map(deal => `'${deal.id}'`);
-        console.log("HEEERRE")
-        console.log(dealIds.length)
-    
-        const pool = postgresPool()
+  if (deals.length) {
+    // get association project deal
+    const dealIds = deals.map((deal) => `'${deal.id}'`);
 
-        return pool.query(`SELECT * FROM project_deal_association  WHERE deal_id IN (${ dealIds.join(',') })`, 
-        (error, results) => {
-            if (error) {
-                console.log("in here")
-              return false;
-            }
-            
-           if(results.rows.length) {
-               console.log("rre hEREEE")
-               console.log(results.rows.length)
-               const projectIds = results.rows.map(dealAssociation => dealAssociation.project_id)
-                console.log(`[[ PROJECT IDs ]]`, projectIds) 
-                return collection.actions.find({ 
-                    metaId : { $in: metaIds },
-                    isDone: false 
-                }).toArray(async function (_, docs) {
-                    console.log("ICI")
+    console.log("[[ DEAL IDs ]]", dealIds);
 
-                    if(docs){
-                        if(docs.length) {
-                            console.log(`[[ ACTION TO UPDATE ]]`, docs)
-                            try {
-                                if(!process.argv[2]){
-                                    await collection.actions.updateMany(
-                                        { metaId : { $in: metaIds } },
-                                        { $set: { isDone: true } },
-                                        { multi: true }
-                                    )
-        
-                                    console.log(`>>>>>>>>>>>> UPDATE ACTIONS SUCCESS`)
-                                }
-    
-                                collection.mongo.close();
-                                return true;
-                            } catch (e) {
-                                console.log(`<<<<<<<<<<< ERROR UPDATE ACTION`)
-                                collection.mongo.close();
-                                return false
-                            } 
-                        }
+    const pool = postgresPool();
+
+    return pool.query(
+      `SELECT * FROM project_deal_association WHERE deal_id IN (${dealIds.join(
+        ","
+      )})`,
+      (error, results) => {
+        console.log("[[ project_deal_association ]] ");
+
+        if (error) {
+          console.log("[[ WITH ERRORS ]]", error);
+          return false;
+        }
+
+        if (results.rows.length) {
+          console.log(
+            "[[ project_deal_association length ]] ",
+            results.rows.length
+          );
+
+          const projectIds = results.rows.map(
+            (dealAssociation) => dealAssociation.project_id
+          );
+
+          console.log(`[[ PROJECT IDs ]]`, projectIds);
+
+          return collection.actions
+            .find({
+              projectId: { $in: projectIds },
+              metaId: { $in: metaIds },
+              isDone: false,
+            })
+            .toArray(async function (_, docs) {
+              console.log("ICI");
+
+              if (docs) {
+                if (docs.length) {
+                  console.log(`[[ ACTION TO UPDATE ]]`, docs);
+                  try {
+                    if (!process.argv[2]) {
+                      await collection.actions.updateMany(
+                        { metaId: { $in: metaIds } },
+                        { $set: { isDone: true } },
+                        { multi: true }
+                      );
+
+                      console.log(`>>>>>>>>>>>> UPDATE ACTIONS SUCCESS`);
                     }
-                    console.log(`<<<<<<<<<<< NO ACTION UPDATED`)
+
+                    collection.mongo.close();
+                    return true;
+                  } catch (e) {
+                    console.log(`<<<<<<<<<<< ERROR UPDATE ACTION`);
                     collection.mongo.close();
                     return false;
-                })
-           }
+                  }
+                }
+              }
+              console.log(`<<<<<<<<<<< NO ACTION UPDATED`);
+              collection.mongo.close();
+              return false;
+            });
+        }
 
-            console.log('<<<<<<<<<<< NO PROJECT DEAL ASSOCIATION FOUND IN POSTGRES')
-            return false;
-        })
-    
-    }
+        console.log(
+          "<<<<<<<<<<< NO PROJECT DEAL ASSOCIATION FOUND IN POSTGRES"
+        );
+        return false;
+      }
+    );
+  }
 
-    console.log('<<<<<<<<<<< NO DEALS FOUND IN HUBSPOT')
-    return false;
-}
+  console.log("<<<<<<<<<<< NO DEALS FOUND IN HUBSPOT");
+  return false;
+};
--- hubspot.js
+++ hubspot.js
@@ -1,28 +1,47 @@
-const { 
-    hubspotClient,
- } = require('./dbConnect')
+const { hubspotClient } = require("./dbConnect");
 
 const getDeals = async (pipelineId, isNext, after = 0) => {
-    const filter = { propertyName: 'pipeline', operator: 'EQ', value: +pipelineId }
-    const filterGroup = { filters: [filter] }
+  const filter = {
+    propertyName: "pipeline",
+    operator: "EQ",
+    value: +pipelineId,
+  };
+  const filterGroup = { filters: [filter] };
 
-    const next =  isNext ? { after } : {}
+  const next = isNext ? { after } : {};
 
-    const publicObjectSearchRequest = {
-        filterGroups: [filterGroup],
-        ...next
-    }
+  const publicObjectSearchRequest = {
+    filterGroups: [filterGroup],
+    ...next,
+  };
 
-    const hubspot = hubspotClient();
-    const result = await hubspot.crm.deals.searchApi.doSearch(publicObjectSearchRequest)
- // Dealstage to exclude : 1189824
-    if(result.body.results.length) {
-        console.log('[[ DEAL NAME ]]', result.body.results.filter(deal => deal.properties.dealstage !== "1189824").map(deal => `'${deal.properties.dealname}'`))
+  const hubspot = hubspotClient();
+  const result = await hubspot.crm.deals.searchApi.doSearch(
+    publicObjectSearchRequest
+  );
+  // Dealstage to exclude : 1189824
+  if (result.body.results.length) {
+    console.log(
+      "[[ DEAL NAME ]]",
+      result.body.results
+        .filter((deal) => deal.properties.dealstage !== "1189824")
+        .map((deal) => `'${deal.properties.dealname}'`)
+    );
+    console.log(
+      "[[ FROM DIFFERENT PIPELINE ]]",
+      result.body.results.filter(
+        (deal) => +deal.properties.pipeline !== +pipelineId
+      )
+    );
+    return [
+      ...result.body.results.filter(
+        (deal) => deal.properties.dealstage !== "1189824"
+      ),
+      ...(await getDeals(pipelineId, true, after + 10)),
+    ];
+  }
 
-        return [...result.body.results.filter(deal => deal.properties.dealstage !== "1189824") , ...(await getDeals(pipelineId, true, after + 10))]
-    }
+  return [];
+};
 
-    return [];
-}
-
-exports.getDeals = getDeals
+exports.getDeals = getDeals;
